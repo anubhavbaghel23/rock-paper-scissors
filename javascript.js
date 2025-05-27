c6e8f4f8b1cd6ea8ce9d772b0e1f15999bf5b54a
Anubhav Baghel <136244141+anubhavbaghel23@users.noreply.github.com.>
2025-05-27 23:03:28 +0530

    code for rock paper scissors game

--- javascript.js
+++ javascript.js
@@ -1,40 +1,108 @@
-// 1. generate a random number
-// 2. Based on that number return "Rock", "paper" or "Scissors"
-
-//Used function to generate random integer
+//This function will generate a random integer
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-//Create a new function named getComputerChoice
+//This function will assign the generated
+// integer to rock,paper or scissors
 function getComputerChoice(){
     let randomNumber = getRandomInt(3);
-    
-    if (randomNumber === 0){
-        return "Rock";
-    } else if (randomNumber === 1){
-        return "scissors";
-    } else {
-        return "Paper";
+
+    if (randomNumber === 0 ){
+        return "rock";
+    } else if(randomNumber === 1){
+        return "paper";
+    } else{
+        return "scissor";
     }
 }
 
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
-
+//This function will ask for Human Input
 function getHumanChoice(){
-    return prompt("Choose Rock, Paper, or Scissors:");
+    let humanInput = prompt("Enter your value (Rock,Paper or Scissor) : ") 
+    let processedInput = humanInput.toLowerCase();
+    return processedInput;
 }
 
-let userChoice = getHumanChoice();
+//This function sets up how one round will be played
 
-console.log("You chose : " + userChoice);
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice){
+function playRound(){
     
+    let humanChoice = getHumanChoice();
+
+    let computerChoice = getComputerChoice();
+
+    if (humanChoice === "rock"){
+        if (computerChoice === "rock"){
+            console.log("Its a tie");
+            return "tie";
+        } else if (computerChoice === "paper"){
+            console.log("You Lost! Paper beats Rock");
+            return "computer";
+        } else /*scissor case */{
+            console.log("You won! Rock beats scissor");
+            return "human";
+        }
+    } else if (humanChoice === "paper"){
+        if (computerChoice === "rock"){
+            console.log("You Won! Paper beats Rock");
+            return "human";
+        } else if (computerChoice === "paper"){
+            console.log("Its a tie");
+            return "tie";
+        } else /*scissor case */{
+            console.log("You Lost! Scissor beats paper");
+            return "computer";
+        }
+    } else if (humanChoice === "scissor"){
+        if (computerChoice === "rock"){
+            console.log("You Lost! Rock beats scissor");
+            return "computer";
+        } else if (computerChoice === "paper"){
+            console.log("You Won! Scissors beats paper");
+            return "human";
+        } else /*scissor case */{
+            console.log("Its a tie");
+            return "tie";
+        }
+    } else {
+        console.log("Invalid Input. Please try again!");
+    }
 }
 
+//This function sets up how the whole game will be played
+function playGame(){
+    
+    let computerScore = 0;
+    let humanScore = 0;
+    let gamesPlayed = 0;
 
+    while (gamesPlayed < 5){
+
+        let result = playRound();
+
+        if (result === "human"){
+            humanScore++;
+            gamesPlayed++;
+        } else if(result === "computer"){
+            computerScore++;
+            gamesPlayed++;
+        } else {
+            gamesPlayed++;
+        }
+    }
+
+    console.log("computer score : " + computerScore);
+    console.log("human score : " + humanScore);
+    console.log("games played : " + gamesPlayed);
+
+    if(computerScore > humanScore){
+        console.log("You Lost! Better luck next time. :(")
+    } else if (humanScore > computerScore){
+        console.log("Hurray! You won the game :D")
+    } else {
+        console.log("Its a tie game")
+    }
+}
+
+playGame();
